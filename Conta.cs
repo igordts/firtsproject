2f74c34bb6a169fc5d1ce4fff83c7642b6a418a4
Igor Sena <gunsigoroses@gmail.com>
2020-05-16 23:06:39 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoJequiti
+{
+    class Conta
+    {
+        private int Agencia;
+        private int Numero;
+        private double Saldo;
+
+        public void SetAgencia(int Valor)
+        { 
+        Agencia = Valor;
+        }
+        public int GetAgencia()
+        {
+        return Agencia;
+        }
+
+        public void SetNumero(int Valor)
+        {
+        Numero = Valor;
+        }
+        public int GetNumero()
+        {
+        return Numero;
+        }
+
+        public void SetSaldo(int Valor)
+        {
+            Saldo = Valor;
+        }
+        public double GetSaldo()
+        {
+            return this.Saldo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BancoJequiti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+            c = new Conta();
+
+            Console.WriteLine("Informe a Agência: ");
+            c.SetAgencia(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Informe o Número: ");
+            c.SetNumero(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Informe o Saldo: ");
+            c.SetSaldo(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Agência informada: " + c.GetAgencia());
+            Console.WriteLine("Número informado: " + c.GetNumero());
+            Console.WriteLine("Saldo informado: " + c.GetSaldo());
+
+            Console.ReadKey();
+        }
+    }
+}
+
